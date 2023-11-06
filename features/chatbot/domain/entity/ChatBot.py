3f7a1a167bb301c5e7aa5b07b3349d9278ff6f65
luis.map <luismap2508@gmail.com>
2023-11-06 08:41:24 +0100

    [refac] adding corret type for chat_history

--- features/chatbot/domain/entity/ChatBot.py
+++ features/chatbot/domain/entity/ChatBot.py
@@ -1,7 +1,7 @@
-from typing import List
+from typing import List, Tuple
 from pydantic import BaseModel
 
 class ChatBot(BaseModel):
     question: str
-    chat_history: List[(str,str)]
+    chat_history: List[Tuple[str,str]]
     model_use: str

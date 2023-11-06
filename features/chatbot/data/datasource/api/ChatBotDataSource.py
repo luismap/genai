39003467fe3e8ad5eb6f82af6f00c64cb8a52565
luis.map <luismap2508@gmail.com>
2023-11-06 09:31:18 +0100

    [refac] updating base chatbot class

--- features/chatbot/data/datasource/api/ChatBotDataSource.py
+++ features/chatbot/data/datasource/api/ChatBotDataSource.py
@@ -1,11 +1,10 @@
 
 from abc import ABC, abstractmethod
-from pydantic import BaseModel
 
 from features.chatbot.data.models.ChatBotModel import ChatBotReadModel
 
 
 class ChatBotDataSource(ABC):
     @abstractmethod
-    def generate_answer(question: str) -> ChatBotReadModel:
+    def generate_base_answer(question: str) -> ChatBotReadModel:
         pass

37f49bdd9568452b223e0da9794655ce397e0a82
luis.map <luismap2508@gmail.com>
2023-11-07 20:43:05 +0100

    [feat] updating streamlit app

--- features/chatbot/presentation/Streamlit.py
+++ features/chatbot/presentation/Streamlit.py
@@ -2,11 +2,17 @@ import streamlit as st
 from core.llm.models.configs.BitsAndBytes import BitsAndBytesConfig
 from features.chatbot.data.controller.ChatBotController import ChatBotController
 from features.chatbot.data.datasource.Llama2DataSource import Llama2DataSource
+from features.chatbot.domain.usecase.InteractiveChat import InteractiveChat
 import transformers
-
+import uuid
+import datetime
 import gc
 
-from features.chatbot.domain.usecase.InteractiveChat import InteractiveChat
+#create session state for rows
+if "rows" not in st.session_state:
+    st.session_state["rows"] = []
+
+rows_collection = []
 
 st.set_page_config(layout="wide")
 st.title('📱Chatbot')
@@ -20,6 +26,7 @@ def initialiaze_model(mode: str):
         st.info("initializing in 8bit")
     elif mode == "4bit":
         bnb_config = BitsAndBytesConfig()
+        st.info("initializing model in 4 bits")
     
     llm_source = Llama2DataSource(bnb_config)
     cb_ctr = ChatBotController([llm_source])
@@ -30,32 +37,53 @@ def initialiaze_model_test(mode: str):
     if mode == "8bit":
         st.info("initializing in 8bit")
     elif mode == "4bit":
-       st.info("initializing in 4bit")
+        st.info("initializing in 4bit")
     return "hi"
 
-def chat(input_text):
-    st.info("the output of the llm")
 
+def add_row(content):
+    st.session_state["rows"].append(content)
+
+def remove_row(content):
+    st.session_state["rows"].remove(content)
+
+def generate_row(content):
+    with chat_content:
+        chat_content.write(content)
+    return content
 
 #sider
 if st.sidebar.button('Initialized 8bit'):
-    ic = initialiaze_model("8bit")
-if st.sidebar.button('Initialized 4bit'):
-    ic = initialiaze_model("4bit")
+    ic = initialiaze_model_test("8bit")
+else:
+    st.sidebar.button('Initialized 4bit')
+    ic = initialiaze_model_test("4bit")
+
+def chat(input_text):
+    #st.info("asking llm")
+    ic.ask_me_something(input_text)
+    #add_row(f"hi {datetime.datetime.now()}")
+    #st.info("the output of the llm")
 
 #main
-col1, col2 = st.columns(2)
+col1_chat, col2_chat_rag = st.columns(2)
 
-with col1.form('chat'):
+
+
+with col1_chat.form('chat'):
     text = st.text_area('Ask me something:', 'give me a list of animals?')
     submitted = st.form_submit_button('Submit')
     if submitted:
-        ic.chat(text)
-"""
-with col2.form('chat_rag'):
-    text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+        chat(text)
+
+with col2_chat_rag.form('chat_rag'):
+    text = st.text_area('Ask me something:', 'What are the three key pieces of advice for learning how to code?')
     submitted = st.form_submit_button('Submit')
     if submitted:
         chat(text)
-"""
 
+chat_content, chat_rag_content = st.columns(2)
+
+for data in st.session_state["rows"][::-1]:
+    row_data = generate_row(data)
+    rows_collection.append(row_data)

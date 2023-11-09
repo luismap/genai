77e0a01d052eb1e9ae00830affcef12cc3ce6740
luis.map <luismap2508@gmail.com>
2023-11-09 15:21:14 +0100

    [feat] adding new features to interactive chat usecase

--- features/chatbot/domain/usecase/InteractiveChat.py
+++ features/chatbot/domain/usecase/InteractiveChat.py
@@ -1,5 +1,6 @@
 
 
+from typing import List
 from features.chatbot.data.controller.ChatBotController import ChatBotController
 from features.chatbot.data.models.ChatBotModel import ChatBotReadModel
 
@@ -12,4 +13,18 @@ class InteractiveChat:
     def ask_me_something(self,question: str) -> ChatBotReadModel:
         answer = self._chatbot_ctr.chat(question)
         return answer
+    
+    def ask_with_rag(self, question: str) -> ChatBotReadModel:
+        answer = self._chatbot_ctr.chat_rag(question)
+        return answer
 
+    def load_text_from_local(self,path: str) -> bool:
+        loaded = self._chatbot_ctr.load_text_from_local(path)
+        return loaded
+
+    def load_from_web(self, links: List[str]) -> bool:
+        web_loaded = self._chatbot_ctr.load_from_web(links)
+        return web_loaded
+
+    def clean_context(self) -> bool:
+        return self._chatbot_ctr.clean_context()

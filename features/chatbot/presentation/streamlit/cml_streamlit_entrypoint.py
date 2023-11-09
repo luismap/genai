03f8a1b93fa9fb9a437680696a9dfc07ad848e63
luis.map <luismap2508@gmail.com>
2023-11-09 08:11:34 +0100

    [feat] adding entrypoint for cdp cml app

---
+++ features/chatbot/presentation/streamlit/cml_streamlit_entrypoint.py
@@ -0,0 +1,8 @@
+import os
+import subprocess
+
+serverport=os.getenv('CDSW_APP_PORT')
+
+sbp = subprocess.Popen(["python3.9","-m", "streamlit", "run", os.path.join(
+            'features', 'chatbot', 'presentation','streamlit', 'StreamlitWithApi.py'), "--server.address=127.0.0.1", f"--server.port={serverport}"],
+                      start_new_session=True)

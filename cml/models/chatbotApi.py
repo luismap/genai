0c84d68ba091e8fb184c27ecbb47d99aba5e9430
luis.map <luismap2508@gmail.com>
2023-11-17 12:12:47 +0100

    [feat] adding chatbot api

---
+++ cml/models/chatbotApi.py
@@ -0,0 +1,47 @@
+from core.llm.models.configs.BitsAndBytes import BitsAndBytesConfig
+from features.chatbot.data.datasource.ChatBotLlama2DataSource import Llama2DataSource
+from features.chatbot.data.controller.ChatBotController import ChatBotController
+from features.chatbot.domain.usecase.InteractiveChat import InteractiveChat
+
+
+bnb_config = BitsAndBytesConfig()
+llm_source = Llama2DataSource(bnb_config)
+chat_ctr = ChatBotController([llm_source])
+ic = InteractiveChat(chat_ctr)
+
+
+def ask(payload):
+    """
+    given a task and a question, produce an answer base on the initialized model
+    payload accepted params:
+    `task`: can be `predict` or `clean_context`
+    clean context will release buffer memory for current chat
+    `question`: question to llm
+    `history`: if we want history return
+
+    example:
+    {
+    "task": "predict",
+    "question": "give me a list of 5 animals? be short in your answer"
+    "history": false
+    },
+    {
+    "task": "clean_context",
+    "question": ""
+    "history": false
+    }
+ 
+    Args:
+        payload (_type_): _description_
+
+    Returns:
+        dict: ChatBotReadModel like dict
+    """
+    if payload["task"] == "predict":
+        question = payload["question"]
+        answer = ic.ask_me_something(question, payload["history"])
+        return answer.dict()
+    if payload["task"] == "clean_context":
+        answer = ic.clean_context()
+        return {"cleaned_context": True}
+    return None

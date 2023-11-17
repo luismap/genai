b8b25f4d8984aefd5357d70aba0a21d959500a3b
luis.map <luismap2508@gmail.com>
2023-11-17 11:56:05 +0100

    [refac] updating interactivechat usecase

--- features/chatbot/domain/usecase/InteractiveChat.py
+++ features/chatbot/domain/usecase/InteractiveChat.py
@@ -10,21 +10,9 @@ class InteractiveChat:
                  chatbot_controller: ChatBotController) -> None:
         self._chatbot_ctr = chatbot_controller
 
-    def ask_me_something(self,question: str) -> ChatBotReadModel:
-        answer = self._chatbot_ctr.chat(question)
+    def ask_me_something(self,question: str, history: bool = False) -> ChatBotReadModel:
+        answer = self._chatbot_ctr.chat(question, history)
         return answer
-    
-    def ask_with_rag(self, question: str) -> ChatBotReadModel:
-        answer = self._chatbot_ctr.chat_rag(question)
-        return answer
-
-    def load_text_from_local(self,path: str) -> bool:
-        loaded = self._chatbot_ctr.load_text_from_local(path)
-        return loaded
-
-    def load_from_web(self, links: List[str]) -> bool:
-        web_loaded = self._chatbot_ctr.load_from_web(links)
-        return web_loaded
 
     def clean_context(self) -> bool:
         return self._chatbot_ctr.clean_context()

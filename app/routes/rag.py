137cd6b19b39fa75375e7aff0acaee9bfed787e7
luis.map <luismap2508@gmail.com>
2023-11-28 14:35:47 +0100

    [feat] adding new enpoints to route rag

--- app/routes/rag.py
+++ app/routes/rag.py
@@ -1,14 +1,11 @@
-
-
-
-
 import logging
 import time
+from typing import List
 from fastapi import APIRouter
 
 from core.utils.MyUtils import MyUtils
 from features.chatbot.data.models.ChatRagModel import ChatRagPayloadModel, ChatRagResponseModel
-import app.batch.RagChatAsyncbatch  as ragchat_ab
+from app.batch.RagChatAsyncbatch import batch_ask, rag_uc
 
 router = APIRouter(
     prefix="/rag",
@@ -22,8 +19,25 @@ logger = logging.getLogger(appProps["logger"])
 @router.post("/ask-rag")
 async def ask_llm(model: ChatRagPayloadModel) -> ChatRagResponseModel:
     start = time.time()
-    crread = await ragchat_ab.batch_ask(model)
+    crread = await batch_ask(model)
     cbresponse = ChatRagResponseModel(**crread.dict())
     end = time.time() - start
     logger.info(f"{cbresponse.question} took: {end} seconds")
     return cbresponse
+
+@router.post("/web-url-src-upload")
+def web_upload(urls: List[str]):
+    return rag_uc.load_from_web(urls)
+
+@router.post("/document-upload")
+def document_upload(path: str):
+    return rag_uc.load_text_from_local(path=path)
+
+@router.post("/vdb-similarity-search")
+def similarity_search(content: str):
+    answer = rag_uc._chatbot_ctr._vector_db.similarity_search(content)
+    return answer
+
+@router.post("/clean-user-context")
+def clean_context(user_id: str):
+    return rag_uc.clean_context(user_id)

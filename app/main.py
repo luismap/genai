ed1e0594e95e03c535e88cff7c5d73b82942d31a
luis.map <luismap2508@gmail.com>
2023-12-12 15:51:11 +0100

    [refac] removing content from the async loops

--- app/main.py
+++ app/main.py
@@ -1,11 +1,7 @@
-import asyncio
 from fastapi import Body, FastAPI
 import logging
 import logging.config
 import yaml
-import app.batch.InteractiveChatAsyncbatch as ichat_ab
-import app.batch.RagChatAsyncbatch as ragchat_ab
-import app.batch.AudioTaskAsyncbatch as audio_ab
 
 from core.utils.MyUtils import MyUtils
 from app.routes import qa, rag, audio
@@ -26,18 +22,6 @@ with open("logging.yaml", 'rt') as f:
 # Note: Do this AFTER the config is loaded above or it won't use the config.
 logger = logging.getLogger(appProps["logger"])
 logger.info("Initial log config in root!")
-
-logger.info("initializing processing loop")
-loop = asyncio.get_event_loop()
-
-logger.info("Starting chatbot batch processing")
-loop.create_task(ichat_ab.batch_processing_loop(loop))
-
-logger.info("Starting ragchatbot processing")
-loop.create_task(ragchat_ab.batch_processing_loop(loop))
-
-logger.info("Starting audio processing")
-loop.create_task(audio_ab.batch_processing_loop(loop))
 
 logger.info("finished awaiting loop")
 

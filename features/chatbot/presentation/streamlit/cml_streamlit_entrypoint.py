5af987786e7c9688c9234e19fc49cab0769343e9
luis.map <luismap2508@gmail.com>
2024-01-10 09:51:05 +0100

    [refac] updating entrypoint for ui

--- features/chatbot/presentation/streamlit/cml_streamlit_entrypoint.py
+++ features/chatbot/presentation/streamlit/cml_streamlit_entrypoint.py
@@ -1,8 +1,2 @@
-import os
-import subprocess
 
-serverport=os.getenv('CDSW_APP_PORT')
-
-sbp = subprocess.Popen(["python3.9","-m", "streamlit", "run", os.path.join(
-            'features', 'chatbot', 'presentation','streamlit', 'StreamlitFastApi.py'), "--server.address=127.0.0.1", f"--server.port={serverport}"],
-                      start_new_session=True)
+!python3.9 -m streamlit run features/chatbot/presentation/streamlit/StreamlitFastApi.py --server.address=127.0.0.1 --server.port=$CDSW_APP_PORT

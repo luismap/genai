415c326bf8f37280381ba69e9eb0c118ce6047c9
luis.map <luismap2508@gmail.com>
2024-02-12 09:58:50 +0100

    [refac] changing indenter

--- features/chatbot/domain/entity/ChatRag.py
+++ features/chatbot/domain/entity/ChatRag.py
@@ -1,12 +1,14 @@
 from typing import List, Tuple
 from pydantic import BaseModel
 
+
 class ChatRag(BaseModel):
     user_id: str
     question: str
-    chat_history: List[Tuple[str,str]] = []
+    chat_history: List[Tuple[str, str]] = []
     model_use: str
     answer: str
+
 
 class ChatRagPayload(BaseModel):
     user_id: str

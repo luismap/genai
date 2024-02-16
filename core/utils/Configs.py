09daa9c72fc5ed4b2cb0af5978c61ef16822e68d
luis.map <luismap2508@gmail.com>
2024-02-16 12:57:43 +0100

    [refac] chaging indenter to ruff

--- core/utils/Configs.py
+++ core/utils/Configs.py
@@ -1,15 +1,15 @@
-
 from pydantic import BaseSettings
 
+
 class Settings(BaseSettings):
-    """Use this class for checking environments vars
-    """
-    #pguser: str
-    #pgpass: str
-    #pgdbname: str
-    #pghost: str
+    """Use this class for checking environments vars"""
+
+    # pguser: str
+    # pgpass: str
+    # pgdbname: str
+    # pghost: str
     hf_token: str
-    #auth, secret generated using openssl rand -hex 32
+    # auth, secret generated using openssl rand -hex 32
     secret_key: str
     algorithm: str
     access_token_expiration_minutes: int

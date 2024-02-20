7bf2c0aef0926ae82b1f8272179f7d57796cdef0
luis.map <luismap2508@gmail.com>
2024-02-20 12:18:04 +0100

    [refac] casting env vars

--- cml/backend/ray_start_cluster_python.py
+++ cml/backend/ray_start_cluster_python.py
@@ -11,12 +11,12 @@ with open("cluster_info.txt", "r") as file:
 worker_start_cmd = f"!export PATH=$PATH:/home/cdsw/.local/bin; ray start --block --address={ray_head_addr}"
 
 
-num_workers = os.environ["num_workers"]
-memory = os.environ["worker_ram_memory"]  # default to 16
-cpu = os.environ["worker_cpu"]  # defaults to 8
-gpu = os.environ[
-    "worker_gpu"
-]  # defaults to 1.  # default to 1. tensors needs a multiple of the attentions layers
+num_workers = int(os.environ["num_workers"])  # defaults to 1
+memory = int(os.environ["worker_ram_memory"])  # default to 16
+cpu = int(os.environ["worker_cpu"])  # defaults to 8
+gpu = int(
+    os.environ["worker_gpu"]
+)  # defaults to 1.  # default to 1. tensors needs a multiple of the attentions layers
 timeout_seconds = 900  # timeout for workers api to wait for resources assingment
 
 ray_workers = cdsw.launch_workers(

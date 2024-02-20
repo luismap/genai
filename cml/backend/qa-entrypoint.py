05829e8e09a93516a59a6e6779ea11e9f9ff981a
luis.map <luismap2508@gmail.com>
2024-02-20 11:33:47 +0100

    [cml] creating initialization of vllm for qa

--- cml/backend/qa-entrypoint.py
+++ cml/backend/qa-entrypoint.py
@@ -1,7 +1,9 @@
 import os
-use_vllm = os.environ["use_vllm"]
-if (use_vllm):
-    !python3 cml/backend/ray_start_cluster_cdsw.py
-else:
-    !uvicorn --app-dir app main-qa:app --port $CDSW_APP_PORT
 
+use_vllm = os.environ["use_vllm"]
+
+if (use_vllm == "true"):
+    !python3 cml/backend/ray_start_cluster_python.py
+    !export PATH=$PATH:/home/cdsw/.local/bin; uvicorn --app-dir app main-qa:app --port $CDSW_APP_PORT
+else:
+    !export PATH=$PATH:/home/cdsw/.local/bin; uvicorn --app-dir app main-qa:app --port $CDSW_APP_PORT

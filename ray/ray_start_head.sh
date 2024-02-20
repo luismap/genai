d9b053e4de4fe2dc3ddedf8017a4ebde4f32552a
luis.map <luismap2508@gmail.com>
2024-02-20 12:19:21 +0100

    [refac] adding readonly port

--- ray/ray_start_head.sh
+++ ray/ray_start_head.sh
@@ -3,10 +3,10 @@
 export PATH=$PATH:/home/cdsw/.local/bin
 
 ray start --head --num-cpus=2 --num-gpus=1 --include-dashboard=true \
---dashboard-port=$CDSW_PUBLIC_PORT
+--dashboard-port=$CDSW_READONLY_PORT
 
 cat /tmp/ray/ray_current_cluster > cluster_info.txt
 
 echo ""
-echo "https://public-$CDSW_ENGINE_ID.$CDSW_DOMAIN"
+echo "https://read-only-$CDSW_ENGINE_ID.$CDSW_DOMAIN"
 

70eed682509018fcfb284ac2274e8f7649d5c48c
luis.map <luismap2508@gmail.com>
2024-02-20 12:44:00 +0100

    [refac] changing indenter to ruff

--- app/batch/InteractiveChatAsyncbatch.py
+++ app/batch/InteractiveChatAsyncbatch.py
@@ -7,7 +7,10 @@ import concurrent
 
 from core.llm.models.configs.BitsAndBytes import BitsAndBytesConfig
 from features.chatbot.data.datasource.ChatBotLlama2DataSource import Llama2DataSource
-from features.chatbot.data.models.ChatBotModel import ChatBotPayloadModel, ChatBotReadModel
+from features.chatbot.data.models.ChatBotModel import (
+    ChatBotPayloadModel,
+    ChatBotReadModel,
+)
 from features.chatbot.data.controller.ChatBotController import ChatBotController
 from features.chatbot.domain.usecase.InteractiveChat import InteractiveChat
 
@@ -26,7 +29,7 @@ wait_time = 3.0
 canlog = True
 appProps = MyUtils.load_properties("general")["app"]
 
-with open("logging.yaml", 'rt') as f:
+with open("logging.yaml", "rt") as f:
     config = yaml.safe_load(f.read())
     logging.config.dictConfig(config)
 
@@ -35,80 +38,86 @@ with open("logging.yaml", 'rt') as f:
 logger = logging.getLogger(appProps["logger"])
 logger.info("Initial log config in route post!")
 
-#core setup for async
+# core setup for async
 language_code_map = ["english", "spanish"]
 
+
 async def batch_ask(cbpm: ChatBotPayloadModel) -> Awaitable[ChatBotReadModel]:
-	if not global_loop:
-		raise ValueError("The global loop has not initialized for translation.")
+    if not global_loop:
+        raise ValueError("The global loop has not initialized for translation.")
 
-	job_future: concurrent.Future[ChatBotReadModel] = global_loop.create_future()
+    job_future: concurrent.Future[ChatBotReadModel] = global_loop.create_future()
 
-	logger.info(f"my future type: {type(job_future)}")	
-	await queue.put((job_future, cbpm))
-	
-	return await job_future
+    logger.info(f"my future type: {type(job_future)}")
+    await queue.put((job_future, cbpm))
+
+    return await job_future
 
 
 async def vector_search(vector):
     # simulate I/O call (e.g. Vector Similarity Search using a VectorDB)
     await asyncio.sleep(1.005)
 
+
 def initialize_model():
-	global model
-	model = "33"
+    global model
+    model = "33"
+
 
 def clean_user_context(user_id: str) -> bool:
-	return ic.clean_context(user_id=user_id)
+    return ic.clean_context(user_id=user_id)
+
 
 def predict(contexts):
-	texts = []
-	for context in contexts:
-		texts.append(model + context["text"])
+    texts = []
+    for context in contexts:
+        texts.append(model + context["text"])
 
-	ic.ask_me_something(texts)
+    ic.ask_me_something(texts)
 
-	return texts
+    return texts
 
-#pool_model = concurrent.futures.ProcessPoolExecutor(max_workers=1, initializer=initialize_model)
 
-def process_batch(batch: List[Tuple[Awaitable[ChatBotReadModel], ChatBotPayloadModel]]) -> None:
-	jobs_future, cbpms = zip(*batch)
+# pool_model = concurrent.futures.ProcessPoolExecutor(max_workers=1, initializer=initialize_model)
 
-	logger.info(f"object before asking llm: {type(cbpms)}")
-	response = ic.ask_me_something(cbpms)
-	
-	for idx in range(len(response)):
-		logger.info(f"object before sending future: {type(response[idx])}")
-		jobs_future[idx].set_result(response[idx])
-		queue.task_done()
+
+def process_batch(
+    batch: List[Tuple[Awaitable[ChatBotReadModel], ChatBotPayloadModel]],
+) -> None:
+    jobs_future, cbpms = zip(*batch)
+
+    logger.info(f"object before asking llm: {type(cbpms)}")
+    response = ic.ask_me_something(cbpms)
+
+    for idx in range(len(response)):
+        logger.info(f"object before sending future: {type(response[idx])}")
+        jobs_future[idx].set_result(response[idx])
+        queue.task_done()
+
 
 async def batch_processing_loop(loop: asyncio.AbstractEventLoop):
-	global queue
-	global global_loop
-	
-	if not queue:
-		logger.debug("Setting the global queue and async loop.")
-		queue = asyncio.Queue(loop=loop, maxsize=max_batch_size)
-		global_loop = loop
+    global queue
+    global global_loop
 
-	#with concurrent.futures.ThreadPoolExecutor(max_workers=1) as pool:
-	with concurrent.futures.ThreadPoolExecutor(max_workers=1) as pool:
-		while True:
-			logger.info("Starting a batch processing")
-			current_batch: List[Tuple[Awaitable[ChatBotReadModel],ChatBotPayloadModel]] = [await queue.get()]
+    if not queue:
+        logger.debug("Setting the global queue and async loop.")
+        queue = asyncio.Queue(loop=loop, maxsize=max_batch_size)
+        global_loop = loop
 
-			while (
-				len(current_batch) < max_batch_size 
-				and not queue.empty()
-			):
-				try:
-					current_batch.append(await asyncio.wait_for(
-						queue.get(), wait_time
-					))
-				except TimeoutError:
-					logger.info("batch timeout")
-					break
-				
-			logger.info(f"Processing batch of size {len(current_batch)}")
-			await loop.run_in_executor(pool, process_batch, current_batch)
+    # with concurrent.futures.ThreadPoolExecutor(max_workers=1) as pool:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=1) as pool:
+        while True:
+            logger.info("Starting a batch processing")
+            current_batch: List[
+                Tuple[Awaitable[ChatBotReadModel], ChatBotPayloadModel]
+            ] = [await queue.get()]
+
+            while len(current_batch) < max_batch_size and not queue.empty():
+                try:
+                    current_batch.append(await asyncio.wait_for(queue.get(), wait_time))
+                except TimeoutError:
+                    logger.info("batch timeout")
+                    break
+
+            logger.info(f"Processing batch of size {len(current_batch)}")
+            await loop.run_in_executor(pool, process_batch, current_batch)

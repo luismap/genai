8289ae9874304db030fc90fb5cac91e076c5aac8
luis.map <luismap2508@gmail.com>
2024-02-23 11:27:31 +0100

    [feat] preparing cml rag entry point to handle single and cluster mode

--- cml/backend/rag-entrypoint.py
+++ cml/backend/rag-entrypoint.py
@@ -1,1 +1,9 @@
-!uvicorn --app-dir app main-rag:app --port $CDSW_APP_PORT
+import os
+
+use_vllm = os.environ["use_vllm"]
+
+if (use_vllm == "true"):
+    !python3 cml/backend/ray_start_cluster_python.py
+    !export PATH=$PATH:/home/cdsw/.local/bin; uvicorn --app-dir app main-rag:app --port $CDSW_APP_PORT
+else:
+    !export PATH=$PATH:/home/cdsw/.local/bin; uvicorn --app-dir app main-rag:app --port $CDSW_APP_PORT

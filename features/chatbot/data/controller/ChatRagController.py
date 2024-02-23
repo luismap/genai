e9b6d94b9f244d7696f33d1def6193a4825bf78c
luis.map <luismap2508@gmail.com>
2024-02-23 09:16:11 +0100

    [refac] switching to ruff formatter

--- features/chatbot/data/controller/ChatRagController.py
+++ features/chatbot/data/controller/ChatRagController.py
@@ -1,12 +1,15 @@
-
-
 import logging
 from typing import List
 from core.utils.MyUtils import MyUtils
 from features.chatbot.data.datasource.RagLlama2DataSource import RagLlama2DataSource
-from features.chatbot.data.datasource.api.RagChatBotDataSource import RagChatBotDataSource
+from features.chatbot.data.datasource.api.RagChatBotDataSource import (
+    RagChatBotDataSource,
+)
 from features.chatbot.data.datasource.api.VectorDbSource import VectorDbSource
-from features.chatbot.data.models.ChatRagModel import ChatRagPayloadModel, ChatRagReadModel
+from features.chatbot.data.models.ChatRagModel import (
+    ChatRagPayloadModel,
+    ChatRagReadModel,
+)
 from features.chatbot.domain.controller.ChatRagControllerABC import ChatRagControllerABC
 
 
@@ -18,24 +21,25 @@ class ChatRagController(ChatRagControllerABC):
     Given a list of `ChatbotDataSource` it will check for the first
     available one, and will use it for inference
     """
-    def __init__(self,
-                 datasources: List[RagChatBotDataSource]
-                 ) -> None:
+
+    def __init__(self, datasources: List[RagChatBotDataSource]) -> None:
         self._app_props = MyUtils.load_properties("general")["app"]
         self._app_state = self._app_props["env"]
         self._logger = logging.getLogger(self._app_props["logger"])
-        self._chat_datasource = MyUtils.first(datasources, lambda ds: ds.is_available == True)
+        self._chat_datasource = MyUtils.first(
+            datasources, lambda ds: ds.is_available == True
+        )
         self._vector_db: VectorDbSource = self._chat_datasource._vector_db
 
         self._logger.info("chatrag controller initialized")
 
         return None
 
-    def chat_rag(self,crpms = List[ChatRagPayloadModel]) -> List[ChatRagReadModel]:
+    def chat_rag(self, crpms=List[ChatRagPayloadModel]) -> List[ChatRagReadModel]:
         answers = self._chat_datasource.chat_rag(crpms)
         return answers
 
-    def load_text_from_local(self,path: str) -> bool:
+    def load_text_from_local(self, path: str) -> bool:
         loaded = self._vector_db.load_text_from_local(path)
         return loaded
 
@@ -51,7 +55,9 @@ class ChatRagController(ChatRagControllerABC):
         parse_tuples = lambda t: t[0] + t[1]
         if user_id in self._chat_datasource._users:
             user_history = self._chat_datasource._user_info[user_id]["history"]
-            history = " ".join([parse_tuples(question_pair) for question_pair in user_history])
+            history = " ".join(
+                [parse_tuples(question_pair) for question_pair in user_history]
+            )
             context_length = len(history)
         else:
             context_length = 0

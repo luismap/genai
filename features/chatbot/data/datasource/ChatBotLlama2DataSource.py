d8533236da49d0470ffb3f8c9264779d64a44908
luis.map <luismap2508@gmail.com>
2024-02-26 15:39:22 +0100

    [refac] removing extra newline

--- features/chatbot/data/datasource/ChatBotLlama2DataSource.py
+++ features/chatbot/data/datasource/ChatBotLlama2DataSource.py
@@ -139,7 +139,7 @@ class Llama2DataSource(ChatBotDataSource):
         Returns:
             str: history formatted
         """
-        combined = ["human: " + q + "\nassistant: " + a + "\n" for q, a in history]
+        combined = ["human: " + q + "\nassistant: " + a for q, a in history]
         return "\n".join(combined)
 
     def chat(self, cbpms=List[ChatBotPayloadModel]) -> List[ChatBotReadModel]:
